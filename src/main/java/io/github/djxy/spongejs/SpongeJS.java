6e5ba297218942a056e309f534524391be6b4b52
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-09-14 09:29:51 -0400

    Check if server can be loaded

--- src/main/java/io/github/djxy/spongejs/SpongeJS.java
+++ src/main/java/io/github/djxy/spongejs/SpongeJS.java
@@ -1,21 +1,23 @@
 package io.github.djxy.spongejs;
 
 import com.eclipsesource.v8.V8;
+import com.google.inject.Inject;
 import io.github.djxy.spongejs.converters.Converter;
 import io.github.djxy.spongejs.module.modules.CommandModule;
 import io.github.djxy.spongejs.module.modules.ConsoleModule;
 import io.github.djxy.spongejs.module.modules.EconomyModule;
 import io.github.djxy.spongejs.module.modules.ServerModule;
 import io.github.djxy.spongejs.util.LibraryLoader;
+import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameAboutToStartServerEvent;
 import org.spongepowered.api.event.game.state.GamePostInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-import java.io.File;
 import java.lang.reflect.Field;
 import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 /**
  * Created by Samuel on 2016-09-07.
@@ -25,14 +27,26 @@ public class SpongeJS {
 
     private Server server;
     private SpongeJSService service;
+    private Path serverPath;
+
+    @Inject
+    private Logger logger;
 
     @Listener
     public void onGamePostInitializationEvent(GamePostInitializationEvent event){
         init();
 
-        System.out.println(FileSystems.getDefault().getPath("nodeJS", "bin", "www").toFile().exists());
+        serverPath = FileSystems.getDefault().getPath("nodeJS", "bin", "www");
 
-        server = new Server(FileSystems.getDefault().getPath("nodeJS", "bin", "www"));
+        if(serverPath.toFile().exists())
+            logger.info("NodeJS server found. Can start it.");
+        else
+            logger.error("NodeJS server not found. Make sure to have " + serverPath.toAbsolutePath()+".");
+
+        if(!serverPath.toFile().exists())
+            return;
+
+        server = new Server(serverPath);
         service  = new SpongeJSService(server);
 
         Sponge.getServiceManager().setProvider(this, SpongeJSService.class, service);
@@ -40,6 +54,9 @@ public class SpongeJS {
 
     @Listener
     public void onGameAboutToStartServerEvent(GameAboutToStartServerEvent event){
+        if(!serverPath.toFile().exists())
+            return;
+        
         server.addModule(new EconomyModule());
         server.addModule(new CommandModule());
         server.addModule(new ConsoleModule());
